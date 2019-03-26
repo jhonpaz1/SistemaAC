1f3d3e5268d1f2ec3768652ff6054821882de98f
jhonpaz1 <jhon.paz@sypsolutions.com>
2019-03-25 19:11:13 -0500

    Agregando controlador Usuarios

---
+++ SistemaAC/Controllers/UsuariosController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SistemaAC.Data;
+using SistemaAC.Models;
+
+namespace SistemaAC.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UsuariosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Usuarios
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.ApplicationUser.ToListAsync());
+        }
+
+        public async Task<List<ApplicationUser>> GetUsuario(string id)
+        {
+            List<ApplicationUser> usuario = new List<ApplicationUser>();
+            var appUsuario = await _context.ApplicationUser.SingleOrDefaultAsync(m => m.Id == id);
+            usuario.Add(appUsuario);
+            return usuario;
+        }
+
+        public async Task<string> EditUsuario(string id, string userName, string email,
+           string phoneNumber, int accessFailedCount, string concurrencyStamp, bool emailConfirmed,
+           bool lockoutEnabled, DateTimeOffset lockoutEnd, string normalizedEmail,
+           string normalizedUserName, string passwordHash, bool phoneNumberConfirmed,
+           string securityStamp, bool twoFactorEnabled, ApplicationUser applicationUser)
+        {
+            var resp = "";
+            try
+            {
+                applicationUser = new ApplicationUser
+                {
+                    Id = id,
+                    UserName = userName,
+                    Email = email,
+                    PhoneNumber = phoneNumber,
+                    EmailConfirmed = emailConfirmed,
+                    LockoutEnabled = lockoutEnabled,
+                    LockoutEnd = lockoutEnd,
+                    NormalizedEmail = normalizedEmail,
+                    NormalizedUserName = normalizedUserName,
+                    PasswordHash = passwordHash,
+                    PhoneNumberConfirmed = phoneNumberConfirmed,
+                    SecurityStamp = securityStamp,
+                    TwoFactorEnabled = twoFactorEnabled,
+                    AccessFailedCount = accessFailedCount,
+                    ConcurrencyStamp = concurrencyStamp
+                };
+                //Actualizamos los datos
+                _context.Update(applicationUser);
+                await _context.SaveChangesAsync();
+                resp = "Save";
+            }
+            catch
+            {
+                resp = "No Save";
+            }
+            return resp;
+        }
+
+        private bool ApplicationUserExists(string id)
+        {
+            return _context.ApplicationUser.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ SistemaAC/wwwroot/js/site.js
@@ -0,0 +1,84 @@
+﻿// Write your JavaScript code.
+$('#modalEditar').on('shown.bs.modal', function () {
+    $('#myInput').focus()
+})
+
+function getUsuario(id, action) {
+    $.ajax({
+        type: "POST",
+        url: action,
+        data: { id },
+        success: function (response) {
+            mostrarUsuario(response);
+        }
+    })
+}
+
+var items;
+//Variables globales por cada propiedad del usuario
+var id;
+var userName;
+var email;
+var phoneNumber;
+//Otras variables donde almacenaremos los datos del registro, pero estos datos no serán modificados
+var accessFailedCount;
+var concurrencyStamp;
+var emailConfirmed;
+var lockoutEnabled;
+var lockoutEnd;
+var normalizedUserName;
+var normalizedEmail;
+var passwordHash;
+var phoneNumberConfirmed;
+var securityStamp;
+var twoFactorEnabled;
+
+function mostrarUsuario(response) {
+    items = response;
+    $.each(items, function (index, val) {
+        $('input[name = Id]').val(val.id);
+        $('input[name = UserName]').val(val.userName);
+        $('input[name = Email]').val(val.email);
+        $('input[name = PhoneNumber]').val(val.phoneNumber);
+    });
+}
+
+function editarUsuario(action) {
+    //Obtenemos los datos del imput respectivo del formulario
+    id = $('input[name=Id]')[0].value;
+    email = $('input[name=Email]')[0].value;
+    phoneNumber = $('input[name=PhoneNumber]')[0].value;
+
+    $.each(items, function (index, val) {
+        accessFailedCount = val.accessFailedCount;
+        concurrencyStamp = val.concurrencyStamp;
+        emailConfirmed = val.emailConfirmed;
+        lockoutEnabled = val.lockoutEnabled;
+        lockoutEnd = val.lockoutEnd;
+        userName = val.userName;
+        normalizedUserName = val.normalizedUserName;
+        normalizedEmail = val.normalizedEmail;
+        passwordHash = val.passwordHash;
+        phoneNumberConfirmed = val.phoneNumberConfirmed;
+        securityStamp = val.securityStamp;
+        twoFactorEnabled = val.twoFactorEnabled;
+    });
+    $.ajax({
+        type: "POST",
+        url: action,
+        data: {
+            id, userName, email, phoneNumber, accessFailedCount,
+            concurrencyStamp, emailConfirmed, lockoutEnabled, lockoutEnd,
+            normalizedEmail, normalizedUserName, passwordHash, phoneNumberConfirmed,
+            securityStamp, twoFactorEnabled
+        },
+        success: function (response) {
+            if (response == "Save") {
+                window.location.href = "Usuarios";
+            }
+            else {
+                alert("No se puede editar los datos del usuario");
+            }
+        }
+    });
+}
